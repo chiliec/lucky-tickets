071534670afc309f04142b37f60291ec2b8748af
Vladimir Babin <vovababin@gmail.com>
2020-10-07 21:04:47 +0500

    Add predicate example with lambda

--- src/main/java/luckyTickets/App.java
+++ src/main/java/luckyTickets/App.java
@@ -1,15 +1,17 @@
 package luckyTickets;
 
 import java.util.Iterator;
+import java.util.function.Predicate;
 
 public class App {
     private static Iterator<Lucky> iterator = new AnotherLuckyIterator();
 
     public static void main(String[] args) {
         long counter = 0L;
+        Predicate<Lucky> predicate = lucky -> lucky.isLucky();
         while (iterator.hasNext()) {
             Lucky next = iterator.next();
-            if (next.isLucky()) {
+            if (predicate.test(next)) {
                 counter++;
                 System.out.println(next);
             }
